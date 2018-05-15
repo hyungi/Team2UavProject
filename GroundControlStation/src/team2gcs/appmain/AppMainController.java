90d42ce24960a45c1545783d672b4ce3760d4976
Kim-SangHoon <Kim-SangHoon@COM-PC-10>
2018-05-15 16:25:53 +0900

    웹뷰 추가

--- GroundControlStation/src/team2gcs/appmain/AppMainController.java
+++ GroundControlStation/src/team2gcs/appmain/AppMainController.java
@@ -15,7 +15,6 @@ import javafx.fxml.Initializable;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Label;
-import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
