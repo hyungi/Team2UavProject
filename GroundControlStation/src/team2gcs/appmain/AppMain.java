12245f48b00da2f95b5e1566d7f921c8858aa9bc
Minki <Minki@DESKTOP-3AGM378>
2018-05-23 09:41:47 +0900

    수정

--- GroundControlStation/src/team2gcs/appmain/AppMain.java
+++ GroundControlStation/src/team2gcs/appmain/AppMain.java
@@ -23,6 +23,7 @@ public class AppMain extends Application{
  		Parent root = FXMLLoader.load(getClass().getResource("appmain.fxml"));
  		scene = new Scene(root);
  		scene.getStylesheets().add(getClass().getResource("../images/app.css").toExternalForm());
+ 		scene.getStylesheets().add(getClass().getResource("../images/tabpane.css").toExternalForm());
 	 	tempScene = scene;
 		primaryStage.setTitle("UAV Ground Control Station(Team2)");
 		primaryStage.setScene(scene);		
