311db4919369613e02becae62a6b303e33b52b94
Minki <Minki@DESKTOP-3AGM378>
2018-06-11 11:49:13 +0900

    time 엔터 가능

--- GroundControlStation/src/team2gcs/camera/viewer/MjpgStreamViewerController.java
+++ GroundControlStation/src/team2gcs/camera/viewer/MjpgStreamViewerController.java
@@ -3,11 +3,15 @@ package team2gcs.camera.viewer;
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayInputStream;
 import java.io.File;
+import java.io.InputStream;
 import java.net.URL;
 import java.util.Date;
+import java.util.Map;
 import java.util.ResourceBundle;
 
 import javax.imageio.ImageIO;
+
+import org.eclipse.paho.client.mqttv3.internal.websocket.Base64;
 
 import gcs.network.Network;
 import javafx.animation.KeyFrame;
@@ -30,6 +34,9 @@ public class MjpgStreamViewerController implements Initializable {
     @FXML private Canvas canvas;
     @FXML private Canvas canvas2;
 	@FXML private Button captureBtn;
+	@FXML private Button startCamBtn;
+	@FXML private Button stopCamBtn;
+	
 	public JSObject jsproxy;
     
     @Override
@@ -37,6 +44,8 @@ public class MjpgStreamViewerController implements Initializable {
         //httpView();
         mqttView();        
         captureBtn.setOnMouseClicked((event) -> {try {handleCaptureBtn(event);} catch (Exception e) {e.printStackTrace();}});
+        startCamBtn.setOnMouseClicked((event) -> {try {handleStartCamBtn(event);} catch (Exception e) {e.printStackTrace();}});
+        stopCamBtn.setOnMouseClicked((event) -> {try {handleStopCamBtn(event);} catch (Exception e) {e.printStackTrace();}});
 		
     }
     /*private void httpView() {
@@ -64,7 +73,7 @@ public class MjpgStreamViewerController implements Initializable {
     }
 
 	public void handleCaptureBtn (MouseEvent event) throws Exception {
-		System.out.println("capture");
+//		System.out.println("capture");
 		Timeline timeline = new Timeline(new KeyFrame(Duration.seconds(0.05), evt -> captureBtn.setVisible(false)),
                 new KeyFrame(Duration.seconds( 0.1), evt -> captureBtn.setVisible(true)));
 		timeline.setCycleCount(1);
@@ -77,5 +86,33 @@ public class MjpgStreamViewerController implements Initializable {
         File output = new File("camera" + new Date().getTime() + ".png");
         ImageIO.write(SwingFXUtils.fromFXImage(snapshot, null), "png", output);
 	}
+
+	public void handleStartCamBtn(MouseEvent event) throws Exception{
+//		System.out.println("StartCam");
+		/*Base64 decoder = new Base64();
+		byte[] pic = decoder.decodeBase64(request.getParameter("pic"));
+		String frameCount = request.getParameter("frame");
+		InputStream in = new ByteArrayInputStream(pic);
+		BufferedImage bImageFromConvert = ImageIO.read(in);
+		String outdir = "output\\"+frameCount;
+		//Random rand = new Random();
+		File file = new File(outdir);
+		if(file.isFile()){
+		    if(file.delete()){
+		        File writefile = new File(outdir);
+		        ImageIO.write(bImageFromConvert, "png", file);
+		    }
+		}*/
+	}
 	
+	public void handleStopCamBtn(MouseEvent event) throws Exception{
+//		System.out.println("StopCam");
+		/*String filePath = "output";
+		File fileP = new File(filePath);
+		String commands = "D:\\ffmpeg-win32-static\\bin\\ffmpeg -f image2 -i "
+		        + fileP + "\\image%5d.png " + fileP + "\\video.mp4";
+		System.out.println(commands);
+		Runtime.getRuntime().exec(commands);
+		System.out.println(fileP.getAbsolutePath());*/
+	}
 }
