5ea27ce706dfa2008da8ae9e24f1a687d7d5e2e6
Administrator <Administrator@COM-PC-10>
2018-06-14 09:04:58 +0900

    a

--- GroundControlStation/src/uav/UavMqttClient.py
+++ GroundControlStation/src/uav/UavMqttClient.py
@@ -22,7 +22,7 @@ vehicle = connect("udp:192.168.3.217:14560", wait_ready=True)
 # vehicle = connect('/dev/ttyS0',wait_ready = True,baud57600) #라즈베리파이에서 실행시 
 
 #MQTT Broker와 연결하기 위한 정보-----------------------------
-mqtt_ip = "192.168.3.16"
+mqtt_ip = "192.168.3.217"
 mqtt_port = 1883
 uav_pub_topic = "/uav2/pub"
 uav_sub_topic = "/uav2/sub"
